4148a0891accb3814976f4c08aaccc83a581a063
Anurag Nadda <anuragn@squadmindsinc.com>
2021-06-23 12:52:58 +0530

    postgres setting

--- Django_Elastic_Search/settings.py
+++ Django_Elastic_Search/settings.py
@@ -76,11 +76,26 @@ WSGI_APPLICATION = 'Django_Elastic_Search.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DB_NAME     = 'elasticsearch'
+DB_USER     = 'manuel'
+DB_PASSWORD = 'jw8s0F4'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER':DB_USER,
+        'PASSWORD':DB_PASSWORD,
+        'HOST':'localhost',
+        'PORT':'5432',
     }
+
 }
 
 
